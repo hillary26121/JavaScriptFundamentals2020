c87bbeea85e14c858806a2c19b30706b162e5c28
hillary26121 <hillaryywallace@gmail.com>
2020-11-06 14:33:04 -0500

    Homework for real

--- exercises/20-promise/promise.js
+++ exercises/20-promise/promise.js
@@ -29,7 +29,32 @@
  * @returns {Promise} which will resolve to the JSON above.
  */
 const getBooksApi = () => {
-  
+    return new Promise( (resolve,reject)=>{
+      setTimeout(() => resolve{
+         {
+            "category": "books",
+            "subCategory": "graphic novels",
+            "data": [{
+              "id": "0c8cb1b2",
+              "title": "Berlin",
+              "authors": ["Jason Lutes"],
+              "image": "https://images-na.ssl-images-amazon.com/images/I/51DhbDeVIVL._SX388_BO1,204,203,200_.jpg"
+            },{
+              "id": "cb195709",
+             "title": "Hey, Kiddo",
+             "authors": ["Jarrett J. Krosoczka"],
+            "image": "https://images-na.ssl-images-amazon.com/images/I/517I7YRvHBL._SX351_BO1,204,203,200_.jpg"
+           },{
+             "id": "77ae31c1",
+             "title": "On a Sunbeam",
+             "authors": ["Tillie Walden"],
+             "image": "https://images-na.ssl-images-amazon.com/images/I/51Ukxxbo-mL._SX359_BO1,204,203,200_.jpg"
+           }]
+           });
+
+        
+      });
+    };
 };
 
 /**
@@ -52,6 +77,10 @@ let data; // Do not change this line.
  * Call on the "getBooksApi()" here and set the variable "data" above
  * to the array of graphic novels that is in your promise's response.
  */
+getBooksApi()
+.then ( (response)=>{
+  data = response.data;
+});
 
 module.exports = {
   getBooksApi,
--- exercises/21-fetch/fetch.js
+++ exercises/21-fetch/fetch.js
@@ -1,5 +1,31 @@
-(function () {
-  /**
+
+
+(function dogPictures() {
+  let button = document.querySelector('#button'); 
+  let picture = document.querySelector('#current-dog');
+  picture.src = 'https://images.dog.ceo/breeds/borzoi/n02090622_4552.jpg';
+  console.log(button);
+  fetch('https://dog.ceo/api/breeds/image/random',{
+    method: 'GET',
+
+  })
+  .then(response => response.json())
+  .then(response =>{
+    picture.src = response.message;
+    console.log(response);
+  
+  });
+  button.addEventListener('click', dogPictures);
+
+
+
+})();
+
+
+
+
+
+ /**
    * As a user, I should be able to click on the a button to see random dog images.
    * Please use fetch in this example.
    *
@@ -12,4 +38,3 @@
    *    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
    * }
    */
-})();
